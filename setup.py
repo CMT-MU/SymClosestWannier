5800b6d5517335a8ade968af6fa3e6abbb16dd1f
HK <hk@meiji.ac.jp>
2023-10-28 01:01:25 +0900

    update

--- setup.py
+++ setup.py
@@ -1,10 +1,3 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
-setup(
-    name="symclosestwannier",
-    version="0.0.1",
-    description="A Python library for Closest Wannier Function Tight-Binding (CW) model based on Plane-Wave (PW) DFT calculation.",
-    author="rikuto oiwa",
-    packages=find_packages(),
-    license="MIT",
-)
+setup()
