f5bfebf89be179c948ebbe2c7231797d05767d8f
RO <65672632+rikuto-oiwa-phys@users.noreply.github.com>
2023-12-16 16:45:58 +0900

    bug fix for tb_gauge

--- symclosestwannier/util/_utility.py
+++ symclosestwannier/util/_utility.py
@@ -126,14 +126,14 @@ def fourier_transform_k_to_r(Ok, kpoints, irvec, atoms_frac=None):
     num_k = kpoints.shape[0]
 
     kR = np.einsum("ka,Ra->kR", kpoints, irvec, optimize=True)
-    phase_R = np.exp(-2 * np.pi * 1j * kR)
+    phase_R = np.exp(+2 * np.pi * 1j * kR)
 
     if atoms_frac is not None:
         tau = np.array(atoms_frac)
         ktau = np.einsum("ka,ma->km", kpoints, tau, optimize=True)
-        eiktau = np.exp(-2 * np.pi * 1j * ktau)
+        eiktau = np.exp(+2 * np.pi * 1j * ktau)
 
-        Or = np.einsum("kR,km,kmn,kn->Rmn", phase_R, eiktau.conjugate(), Ok, eiktau, optimize=True) / num_k
+        Or = np.einsum("kR,km,kmn,kn->Rmn", phase_R, eiktau, Ok, eiktau.conjugate(), optimize=True) / num_k
     else:
         Or = np.einsum("kR,kmn->Rmn", phase_R, Ok, optimize=True) / num_k
 
@@ -166,14 +166,14 @@ def fourier_transform_r_to_k(Or, kpoints, irvec, ndegen=None, atoms_frac=None):
     kpoints = np.array(kpoints, dtype=float)
 
     kR = np.einsum("ka,Ra->kR", kpoints, irvec, optimize=True)
-    phase_R = np.exp(+2 * np.pi * 1j * kR)
+    phase_R = np.exp(-2 * np.pi * 1j * kR)
 
     if atoms_frac is not None:
         tau = np.array(atoms_frac)
         ktau = np.einsum("ka,ma->km", kpoints, tau, optimize=True)
-        eiktau = np.exp(+2 * np.pi * 1j * ktau)
+        eiktau = np.exp(-2 * np.pi * 1j * ktau)
 
-        Ok = np.einsum("R,kR,km,Rmn,kn->kmn", weight, phase_R, eiktau.conjugate(), Or, eiktau, optimize=True)
+        Ok = np.einsum("R,kR,km,Rmn,kn->kmn", weight, phase_R, eiktau, Or, eiktau.conjugate(), optimize=True)
     else:
         Ok = np.einsum("R,kR,Rmn->kmn", weight, phase_R, Or, optimize=True)
 
@@ -368,6 +368,6 @@ def construct_Ok(z, num_wann, kpoints, rpoints, matrix_dict):
     ]
 
     Or = construct_Or(z, num_wann, rpoints, matrix_dict)
-    Ok, _ = fourier_transform_r_to_k(Or, rpoints, kpoints, atoms_frac)
+    Ok = fourier_transform_r_to_k(Or, rpoints, kpoints, atoms_frac)
 
     return Ok
