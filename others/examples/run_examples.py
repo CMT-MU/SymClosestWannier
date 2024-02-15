82cfa00ecf7d2af430d8c9010a61e738468a05b0
RO <65672632+rikuto-oiwa-phys@users.noreply.github.com>
2024-02-16 07:50:21 +0900

    update examples

--- others/examples/run_examples.py
+++ others/examples/run_examples.py
@@ -10,6 +10,7 @@ def main():
     models = sorted([f for f in os.listdir(".") if os.path.isdir(os.path.join(".", f))])
 
     for seedname in models:
+        print(seedname)
         os.chdir(seedname)
         subprocess.run(f"pw2cw {seedname}", shell=True)
         os.chdir("..")
