b307d6b8d5f5f34a45dfde2ae20727e69302a966
RO <65672632+rikuto-oiwa-phys@users.noreply.github.com>
2024-07-18 23:36:03 +0900

    minor change

--- symclosestwannier/cw/cw_info.py
+++ symclosestwannier/cw/cw_info.py
@@ -97,7 +97,7 @@ class CWInfo(dict):
             if name == "umat" and (d["restart"] != "w90"):
                 continue
             if name == "mmn":
-                if not np.any([d["write_rmn"], d["write_vmn"], d["write_tb"], d["berry"]]):
+                if not np.any([d["write_rmn"], d["write_vmn"], d["write_tb"], d["berry"], d["gyrotropic"]]):
                     continue
             if name == "spn":
                 if not np.any(
