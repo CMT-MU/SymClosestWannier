4d85d6652d82f5e2772a2bce4f3ccb344478bfc2
RO <65672632+rikuto-oiwa-phys@users.noreply.github.com>
2025-08-12 18:25:38 +0900

    version up

--- symclosestwannier/util/message.py
+++ symclosestwannier/util/message.py
@@ -53,7 +53,7 @@ def cw_open_msg():
              |     The SymClosestWannier Developer Group and     |
              |        individual contributors                    |
              |                                                   |
-             |        Release: 1.10.9      5th Aug  2025         |
+             |        Release: 1.10.10      5th Aug  2025         |
              |                                                   |
              | This program is free software; you can            |
              | redistribute it and/or modify it under the terms  |
